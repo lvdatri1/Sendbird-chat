cf656567c9bfeedc43a22e66d9fc604fea8fc2e9
Trinh Le <TrinhL@hpa.org.nz>
2019-02-05 09:49:23 +1300

    read

---
+++ precache-manifest.f710f7c4648560955cad11d3e6d86be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb824e38fe5047f17e15",
+    "url": "/Sendbird-chat/static/js/runtime~main.fb824e38.js"
+  },
+  {
+    "revision": "fa80cc1bf5e4cb36db17",
+    "url": "/Sendbird-chat/static/js/main.fa80cc1b.chunk.js"
+  },
+  {
+    "revision": "e296b7c47dce19a82f04",
+    "url": "/Sendbird-chat/static/js/1.e296b7c4.chunk.js"
+  },
+  {
+    "revision": "fa80cc1bf5e4cb36db17",
+    "url": "/Sendbird-chat/static/css/main.7cd50264.chunk.css"
+  },
+  {
+    "revision": "a1206eb55337ce7306679c957967fa20",
+    "url": "/Sendbird-chat/index.html"
+  }
+];
